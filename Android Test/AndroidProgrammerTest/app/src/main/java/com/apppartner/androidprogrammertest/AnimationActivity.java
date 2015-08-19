2ff9022c13e31f5153f0cc3bb381028099af52ab
ShikhaSoni <srs6573@rit.edu>
2015-08-19 16:19:59 -0400

    - touch and drag change

--- Android Test/AndroidProgrammerTest/app/src/main/java/com/apppartner/androidprogrammertest/AnimationActivity.java
+++ Android Test/AndroidProgrammerTest/app/src/main/java/com/apppartner/androidprogrammertest/AnimationActivity.java
@@ -1,10 +1,15 @@
 package com.apppartner.androidprogrammertest;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 
@@ -13,13 +18,38 @@ public class AnimationActivity extends ActionBarActivity implements View.OnTouch
 
     float x, y=0.0f;
     boolean moving =false;
+    ImageView image;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animation);
-        ImageView image= (ImageView)findViewById(R.id.imageView);
+        image=(ImageView)findViewById(R.id.imageView);
         image.setOnTouchListener(this);
+        ImageButton btn=(ImageButton)findViewById(R.id.imageButton2);
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                if(image.getVisibility()==View.VISIBLE)
+                {
+                    boolean toRight = false;
+                    Context c = null;
+                    //Animation out=AnimationUtils.makeOutAnimation(this,true);
+                    Animation out=AnimationUtils.makeOutAnimation(c, toRight);
+                    image.startAnimation(out);
+                    image.setVisibility(View.INVISIBLE);
+
+                } else {
+                    int id = 0;
+                    Context c = null;
+                    //  Animation in = AnimationUtils.loadAnimation(this, android.R.anim.fade_in);
+                    Animation in= AnimationUtils.loadAnimation(c, id);
+
+                    image.startAnimation(in);
+                    image.setVisibility(View.VISIBLE);
+                }
+            }
+        });
     }
 
     @Override
